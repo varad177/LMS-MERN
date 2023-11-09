d9dfffc00a6bbbd14ee7b2796a88ec58a6eb9c0f
Varad Dhumale <varaddhumale177@gmail.com>
2023-11-09 21:11:29 +0530

    new upload code added

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -1,37 +1,33 @@
  
+import multer from 'multer';
+import path from 'path';
 
-import multer from 'multer'
-import path from 'path'
-
-
-const upload = multer({
-
-    limits: { fileSize: 50 * 1024 * 1024 }, //50 mb in size
-    storage: multer.diskStorage({
-        destination: 'uploads/',
-        filename: (_req, file, cb) => {
-            cb(null, file.originalname);
-        },
-    }),
-
-    fileFilter: (_req, file, cb) => {
-        let ext = path.extname(file.originalname);
-
-        if (
-            ext !== ".jpg" &&
-            ext !== ".jpeg" &&
-            ext !== ".webp" &&
-            ext !== ".png" &&
-            ext !== ".mp4"
-        ) {
-
-            cb(new Error(`Unsupported file type! ${ext}`), false)
-            return;
-        }
-        cb(null, true)
-
-    },
-
+const storage = multer.diskStorage({
+  destination: function (_req, _file, cb) {
+    cb(null, 'uploads/'); // Specify the destination directory for uploaded files
+  },
+  filename: function (_req, file, cb) {
+    cb(null, file.originalname); // Use the original file name as the destination file name
+  },
 });
 
-export default upload 
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 50 * 1024 * 1024 }, // 50 MB in size limit
+  fileFilter: (_req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    if (
+      ext !== '.jpg' &&
+      ext !== '.jpeg' &&
+      ext !== '.webp' &&
+      ext !== '.png' &&
+      ext !== '.mp4'
+    ) {
+      cb(new Error(`Unsupported file type! ${ext}`), false);
+      return;
+    }
+    cb(null, true);
+  },
+});
+
+export default upload;
