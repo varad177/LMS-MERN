236de3e6e4a1c5d5d3404a550264f7e2c0055ec2
Varad Dhumale <varaddhumale177@gmail.com>
2023-11-09 21:23:52 +0530

    new code

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -1,27 +1,20 @@
- 
 import multer from 'multer';
 import path from 'path';
 
-const storage = multer.diskStorage({
-  destination: function (_req, _file, cb) {
-    cb(null, 'uploads/'); // Specify the destination directory for uploaded files
-  },
-  filename: function (_req, file, cb) {
-    cb(null, file.originalname); // Use the original file name as the destination file name
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({
-  storage: storage,
-  limits: { fileSize: 50 * 1024 * 1024 }, // 50 MB in size limit
+  limits: { fileSize: 50 * 1024 * 1024 },
+  storage,
   fileFilter: (_req, file, cb) => {
-    const ext = path.extname(file.originalname);
+    let ext = path.extname(file.originalname);
+
     if (
-      ext !== '.jpg' &&
-      ext !== '.jpeg' &&
-      ext !== '.webp' &&
-      ext !== '.png' &&
-      ext !== '.mp4'
+      ext !== ".jpg" &&
+      ext !== ".jpeg" &&
+      ext !== ".webp" &&
+      ext !== ".png" &&
+      ext !== ".mp4"
     ) {
       cb(new Error(`Unsupported file type! ${ext}`), false);
       return;
